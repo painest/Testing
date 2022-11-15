405c1dcd56ad22511714e205979042af7fec7c80
painest <nnnnddd9@hotmail.com>
2022-11-16 02:35:22 +0300

    just test

---
+++ website/src/App.js
@@ -0,0 +1,12 @@
+import Buttons from './Componenet/Buttons'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <Buttons text="HI"></Buttons>
+    </div>
+  );
+}
+
+export default App;
---
+++ website/src/Componenet/Buttons.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Buttons(props) {
+  return (
+    <div>
+      <button className="my_button">{props.text}</button>
+    </div>
+  )
+}
